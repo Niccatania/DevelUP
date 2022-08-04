2586bd0566249b94d9c87a0b4ee310f9728e17e1
OwenMG <36777755+OwenMG@users.noreply.github.com>
2022-08-03 20:53:22 -0700

    continues schema progress, commit to pull

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,17 +2,41 @@ const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
     type User {
-    _id: ID
-    username: String
-    password: String
-    email: String
-    projects: [Project]
+        _id: ID
+        username: String
+        password: String
+        email: String
+        projects: [Project]
     }
+
+    type Service {
+            description: String
+            price: Float
+        }
+
     type Developer {
+        _id: ID
+        name: String
+        image: String
+        about: String
+        services: [Service]
+    }
+
+    type Project {
+        _id: ID
         
     }
-    type Query {
+    
 
+    type Query {
+        user: User
+        allUsers: [User]
+        developer: Developer
+        allDevelopers: [Developer]
+        service: Service
+        allServices: [Service]
+        project: Project
+        allProjects: [Project]
     }
 `;
 module.exports = typeDefs;
