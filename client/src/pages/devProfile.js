e4ef9800f49935ae940ecaa21a949dd65d608f7f
Niccatania <Niccatania6@gmail.com>
2022-08-04 00:23:50 -0700

    Adding Login/ Signup, As weLL as Devcards with props located in the DevDATA

---
+++ client/src/pages/devProfile.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import {
+  Flex,
+  Heading,
+  Input,
+  Button,
+ 
+} from '@chakra-ui/react';
+import { SimpleGrid } from '@chakra-ui/react' 
+const DevProfile = (props) => {
+    return(
+    <div>
+      <SimpleGrid minChildWidth='120px' spacing='40px'>
+{/* Here we use a map to render our devProfile with prop data provided in DevData */}
+      {props.devData.map((dev) => {
+        return (
+          <div key={dev.id}>
+                    <Box bg="gray.400" w="200px" h="200px">
+                      <center>
+                        <h1>{dev.name}</h1>
+                        <img
+                          alt="Developer profile"
+                          src={`${process.env.PUBLIC_URL}${dev.image}`}
+                        />
+                      </center>
+                    </Box>
+                    <Spacer />            
+          </div>    
+        );
+      })}
+         </SimpleGrid>
+</div>
+  );
+};
+
+export default DevProfile;
+
--- client/src/pages/landing.js
+++ client/src/pages/landing.js
@@ -15,55 +15,40 @@ import {
   Divider
 } from "@chakra-ui/react";
 
+import Login from "./login";
+import DevCard from "../components/devCard";
+import DevData from "../components/devData"
+import Navbar from "../components/NavBar";
+
 const Landing = () => {
   return (
+  
     <div>
       <box>
         {" "}
         <Button colorScheme="blue" to="/login">
           Login/Signup
         </Button>
+        
       </box>
 
       <Center>
+        {/* < Navbar /> */}
         <Heading> Fantastic Happiness!</Heading>
       </Center>
       <Divider orientation='horizontal' />
-
+      <div>
       <Center>
         {" "}
-        <h1>Meet the Developers!</h1>
+        <h1 >Meet the Developers!</h1>
       </Center>
-      <Flex>
-        <Box bg="gray.400" w="200px" h="200px">
-          <center>
-            <h1>Tiffany Dempsey</h1>
-            <Avatar name="Tiffany Dempsey" src="" />
-          </center>
-        </Box>
-        <Spacer />
-        <Box bg="gray.400" w="200px" h="200px">
-          <center>
-            <h1>Nic Catania</h1>
-            <Avatar name="Nic Catania" src="" />
-          </center>
-        </Box>
-        <Spacer />
-        <Box bg="gray.400" w="200px" h="200px">
-          <center>
-            <h1>Supriya Sharma</h1>
-            <Avatar name="Supriya Sharma" src="" />
-          </center>
-        </Box>
-        <Spacer />
-        <Box bg="gray.400" w="200px" h="200px">
-          <center>
-            <h1>Owen Greengo</h1>
-            <Avatar name="Owen Greengo" src="" />
-          </center>
-        </Box>
-      </Flex>
+      </div>
+
+     <div>
+      < DevData />
+     </div>
     </div>
+    
   );
 };
 
