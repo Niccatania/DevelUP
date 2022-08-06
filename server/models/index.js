20285fc117e35645689c025105f0395f87c7bbb0
OwenMG <36777755+OwenMG@users.noreply.github.com>
2022-08-06 09:34:14 -0700

    adds effective seed data and query all__ queries

--- server/models/index.js
+++ server/models/index.js
@@ -1,5 +1,6 @@
 const User = require('./User');
 const Developer = require('./Developer');
 const Project = require('./Project');
+const Service = require('./Service');
 
-module.exports = { User, Developer, Project };
+module.exports = { User, Developer, Project, Service };
