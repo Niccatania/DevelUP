53aca1f1e34dd327c0f0424d1b5410be96f88698
Tiffany Dempsey <tiffany.dempsey2000@gmail.com>
2022-08-06 11:29:36 -0700

    merge conflicts

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import "./App.css";
 
-<<<<<<< HEAD
 // import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 // import {
@@ -12,26 +11,44 @@ import "./App.css";
 
 import Landing from "./pages/landing";
 import Login from "./pages/services";
-=======
+import { setContext } from '@apollo/client/link/context';
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  createHttpLink,
-} from "@apollo/client";
+// import {
+//   ApolloClient,
+//   InMemoryCache,
+//   ApolloProvider,
+//   createHttpLink,
+// } from "@apollo/client";
 
 import Landing from "./pages/landing";
+
 import Login from "./pages/servicesPage";
->>>>>>> 8ab7758a (profile and checkout page)
+
+// import Login from "./pages/services";
+
 import NavBar from "./components/NavBar";
+// import Cart from "./components/Cart";
+import Profile from "./pages/profile";
+// import DevProfile from "./devProfile";
 
 import { ChakraProvider } from "@chakra-ui/react";
 
 // const httpLink = createHttpLink({
-//   uri: '/graphql',
+//   uri: "/graphql",
 // });
+
+// const authLink = setContext((_, { headers }) => {
+//   const token = localStorage.getItem('id_token');
+//   return {
+//     headers: {
+//       ...headers,
+//       authorization: token ? `Bearer ${token}` : '',
+//     },
+//   };
+// });
+
 
 // const client = new ApolloClient({
 //   link: authLink.concat(httpLink),
@@ -41,9 +58,24 @@ import { ChakraProvider } from "@chakra-ui/react";
 function App() {
   return (
     //Wrap application in chakra provider at root(TIffany)
+    // <ApolloProvider client={client}>
+    //   <Router>
+    //     <div>
+    //       <ChakraProvider>
+    //         <NavBar />
+    //         <Routes>
+    //           <Route path="/" element={<Landing />} />
+    //           <Route path="/login" element={<login />} />
+    //           <Route path="/profile" element={<profile />} />
+    //         </Routes>
+    //       </ChakraProvider>
+    //     </div>
+    //   </Router>
+    // </ApolloProvider>
+
     <div>
       <ChakraProvider>
-        <Landing />
+        <Profile />
       </ChakraProvider>
     </div>
   );
--- client/src/pages/profile.js
+++ client/src/pages/profile.js
@@ -13,26 +13,88 @@ import {
   Heading,
   Avatar,
   Divider,
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  TableContainer,
 } from "@chakra-ui/react";
 
-const profile = () =>  {
-    return (
-        <div>
-        <Center> 
-            <Heading> Client Profile </Heading>
-        </Center>
-        <Avatar> User Avitar</Avatar>
-
-        <Box>
-            <Flex>
-
-            </Flex>
-        </Box>
-        </div>
-
-    );
+const profile = () => {
+  return (
+    <div>
+      <Center>
+        <Avatar w="10rem" h="10rem" p="2%" m ="3%">
+          {" "}
+          Username
+        </Avatar>
+      </Center>
+      <Center>
+        <Heading>Your Projects</Heading>
+        <Divider></Divider>
+      </Center>
+      <TableContainer>
+        <Table variant="simple">
+          {/* Link button to Services Page */}
+          <TableCaption>
+            {" "}
+            <Button>Start New Project</Button>
+          </TableCaption>
+          <Thead>
+            <Tr>
+              <Th>Project Title</Th>
+              <Th>Status</Th>
+              <Th isNumeric>Cost</Th>
+              <Th> Paid Status</Th>
+              <Th>Developer(s)</Th>
+              <Th>Make Payment</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            <Tr>
+              <Td>Site Title</Td>
+              <Td>Complete/ In progress</Td>
+              <Td isNumeric>$400.00</Td>
+              <Td>Not Paid</Td>
+              <Td>Tiffany, Supriya, Nic, Owen </Td>
+              {/* link to Cart */}
+              <Td>
+                {" "}
+                <Button>Make Payment</Button>
+              </Td>
+            </Tr>
+            <Tr>
+              <Td>Site Title</Td>
+              <Td>Complete/ In progress</Td>
+              <Td isNumeric>$400.00</Td>
+              <Td> Paid </Td>
+              <Td>Tiffany, Supriya, Nic, Owen </Td>
+              {/* link to Cart */}
+              <Td>
+                {" "}
+                <Button>Make Payment</Button>
+              </Td>
+            </Tr>
+            <Tr>
+              <Td>Site Title</Td>
+              <Td>Complete/ In progress</Td>
+              <Td isNumeric>$800.00</Td>
+              <Td> Not Paid</Td>
+              <Td>Tiffany, Supriya, Nic, Owen </Td>
+              {/* link to Cart */}
+              <Td>
+                {" "}
+                <Button>Make Payment</Button>
+              </Td>
+            </Tr>
+          </Tbody>
+        </Table>
+      </TableContainer>
+    </div>
+  );
 };
 
 export default profile;
-
-
