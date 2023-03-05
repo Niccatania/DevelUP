da1f5de7031cab90ae12862cba62b7364786015b
Niccatania <Niccatania6@gmail.com>
2023-03-05 01:11:40 -0600

    add styling for project page, and updated with placeholder services

--- client/src/pages/landing.js
+++ client/src/pages/landing.js
@@ -1,5 +1,4 @@
 import React from "react";
-// import "./index.css";
 import {
   Container,
   Button,
@@ -16,7 +15,6 @@ import {
   Text,
   SimpleGrid
 } from "@chakra-ui/react";
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { Link } from "react-router-dom";
 
 import Login from "./login";
@@ -26,53 +24,87 @@ import Navbar from "../components/NavBar";
 import DevProfile from "./devProfile";
 import Profile from "./clientProfile";
 
+const services = [
+  {
+    name: "Web Design",
+    icon: <i className="fas fa-paint-brush"></i>,
+    description: "Create a beautiful and modern website that represents your brand."
+  },
+  {
+    name: "Web Development",
+    icon: <i className="fas fa-code"></i>,
+    description: "Build a customized and responsive website that meets your specific needs."
+  },
+  {
+    name: "Search Engine Optimization",
+    icon: <i className="fas fa-search"></i>,
+    description: "Optimize your website to rank higher on search engines and attract more visitors."
+  },
+  {
+    name: "Social Media Marketing",
+    icon: <i className="fab fa-instagram"></i>,
+    description: "Promote your brand on social media platforms to increase your online presence."
+  }
+];
+
 const Landing = () => {
   return (
     <div>
+      <div>
+        <Box mt={"125px"} mb={"125px"} ml={"20px"} mr={"20px"}>
+          <Center>
+            <Text
+              bgGradient='linear(to-l, #7928CA, #FF0080)'
+              bgClip='text'
+              fontSize='6xl'
+              fontWeight='extrabold'
+            >
+              DevelUp
+            </Text>
+          </Center>
 
+          <Box textAlign="center" mt={8}>
+            <Text fontSize="2xl" fontWeight="bold">
+              Level up your business, media, or personal brand with a polished unique website.
+            </Text>
+            <Button
+              mt={4}
+              size="lg"
+              colorScheme="pink"
+              borderRadius="full"
+            >
+               <Link as={Link} to="/services/select">
+                            Get Started
+                        </Link>
+          
+            </Button>
+          </Box>
+        </Box>
+      </div>
 
-     
-     <div>
-      
-      <Box mt={"125px"} mb={"125px"} ml={"20px"} mr={"20px"}>
+      <div>
+        <DevData />
+      </div>
 
-    <Center>
-    <Text
-  bgGradient='linear(to-l, #7928CA, #FF0080)'
-  bgClip='text'
-  fontSize='6xl'
-  fontWeight='extrabold'
->
-DevelUp
-</Text>
-</Center>
+      <Box bg="gray.100" py={12}>
+        <Container maxW="6xl" textAlign="center">
+          <Heading as="h2" size="xl" mb={6}>Our Services</Heading>
 
-<Box textAlign="center" mt={8}>
-    <Text fontSize="2xl" fontWeight="bold">
-      Level up your business, media, or personal brand with a polished unique website.
-    </Text>
-    <Button
-    mt={4}
-    size="lg"
-    colorScheme="pink"
-    borderRadius="full"
-  >
-    Get started
-  </Button>
-  
-</Box>
-</Box>
-       
-     </div>
-     
-    <div>
-      
-     < DevData />
-    
+          <SimpleGrid columns={[1, null, 2, 4]} spacing={8}>
+            {services.map(service => (
+              <Box key={service.name} borderWidth="1px" borderRadius="lg" p={6}>
+                <Box fontSize="3xl" mb={4}>
+                  {service.icon}
+                </Box>
+                <Heading as="h3" size="md" mb={4}>{service.name}</Heading>
+                <Text>{service.description}</Text>
+              </Box>
+            ))}
+          </SimpleGrid>
+        </Container>
+      </Box>
     </div>
-   
-   </div>
-  
- );
+  );
 };
+
 export default Landing;
--- client/src/pages/services.js
+++ client/src/pages/services.js
@@ -9,45 +9,81 @@ import {
   Box,
   Divider,
   ButtonGroup,
+  Text,
+  Flex
 } from "@chakra-ui/react";
 
 const Services = () => {
   return (
-    <Box
-      bgGradient="linear(to-l, #7928CA, #FF0080)"
-      minH="100vh"
-    >
-      <Box w="90%" maxW="600px" p={5} mx="auto" my={20} bg="white" borderRadius="lg">
+    <Box bg="gray.100" minH="100vh">
+      <Box bgGradient="linear(to-l, #7928CA, #FF0080)" py={20}>
         <Center>
-          <Heading as="h1" size="lg" mb={5} color="gray.800">
+          <Heading as="h1" size="lg" color="white">
             Select a Service:
           </Heading>
         </Center>
-        <Center>
-          <ButtonGroup
-            display={{ base: "flex", md: "row" }}
-            flexDirection={{ base: "column", md: "row" }}
-            mb={{ base: "1rem", md: "0" }}
-          >
-            <Link to="/services/new-site">
-              <Button colorScheme="purple" size="lg" mb={{ base: "1rem", md: "0" }} _hover={{ boxShadow: "xl" }}>
-                Create a Site From Scratch
-              </Button>
-            </Link>
-            <Link to="/services/refactor-site">
-              <Button colorScheme="purple" size="lg" mb={{ base: "1rem", md: "0" }} _hover={{ boxShadow: "xl" }}>
-                Refactor an Existing Site
-              </Button>
-            </Link>
-            <Link to="/services/custom-request">
-              <Button colorScheme="purple" size="lg" mb={{ base: "1rem", md: "0" }} _hover={{ boxShadow: "xl" }}>
-                Create Custom Request
-              </Button>
-            </Link>
-          </ButtonGroup>
-        </Center>
       </Box>
-    </Box>
+      <Container maxW="container.xl" mt={16} mb={20}>
+  <Flex flexDirection={{ base: "column", md: "row" }} justifyContent="center">
+    <Box bg="white" borderRadius="lg" boxShadow="lg" w={{ base: "80%", md: "25%" }} p={8} m={4}>
+            <Center>
+              <Heading as="h2" size="md" mb={4}>
+                Create a Site From Scratch
+              </Heading>
+            </Center>
+            <Divider />
+            <Text mt={4}>
+              We can create a custom website for your business or personal brand from scratch. Our team of developers will work with you to create a site that reflects your unique style and meets your specific needs.
+            </Text>
+            <Center>
+              <Link to="/services/new-site">
+                <Button colorScheme="pink" size="md" my={4} mx={2}>
+                  Get started
+                </Button>
+              </Link>
+            </Center>
+          </Box>
+
+          <Box bg="white" borderRadius="lg" boxShadow="lg" w={{ base: "80%", md: "25%" }} p={8} m={4}>
+            <Center>
+              <Heading as="h2" size="md" mb={4}>
+                Refactor an Existing Site
+              </Heading>
+            </Center>
+            <Divider />
+            <Text mt={4}>
+              If you already have a website but it needs a new look or some updates, our team can help. We'll work with you to update the design and functionality of your site to better serve your needs and meet your goals.
+            </Text>
+            <Center>
+              <Link to="/services/refactor-site">
+                <Button colorScheme="pink" size="md" my={4} mx={2}>
+                  Get started
+                </Button>
+              </Link>
+            </Center>
+          </Box>
+
+          <Box bg="white" borderRadius="lg" boxShadow="lg" w={{ base: "80%", md: "25%" }} p={8} m={4}>
+            <Center>
+              <Heading as="h2" size="md" mb={4}>
+                Create Custom Request
+              </Heading>
+            </Center>
+            <Divider />
+            <Text mt={4}>
+              If you have a unique request or need that isn't covered by our other services, we can work with you to create a custom solution that meets your specific requirements. Our team has experience working on a wide range of projects and can handle even the most complex requests.
+            </Text>
+            <Center>
+              <Link to="/services/custom-request">
+                <Button colorScheme="pink" size="md" my={4} mx={2}>
+                  Get started
+                </Button>
+              </Link>
+            </Center>
+          </Box>
+        </Flex>
+      </Container>
+      </Box>
   );
 };
 
